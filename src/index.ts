79ab90b354519630babf0790692637e7e4c3db81
Artur <busanix@gmail.com>
2023-07-27 12:37:36 -0300

    dev

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,44 @@
 /* eslint-disable no-restricted-globals */
-import { afterAll, vi } from 'vitest'
+import { afterAll, vi } from "vitest";
 
-global.jest = vi
+global.jest = vi;
 
 declare global {
   // eslint-disable-next-line no-var, vars-on-top
-  var jest: typeof vi | undefined
+  var jest: typeof vi | undefined;
 }
 
 const apis = [
-  'Path2D',
-  'CanvasGradient',
-  'CanvasPattern',
-  'CanvasRenderingContext2D',
-  'DOMMatrix',
-  'ImageData',
-  'TextMetrics',
-  'ImageBitmap',
-  'createImageBitmap',
-] as const
+  "Path2D",
+  "CanvasGradient",
+  "CanvasPattern",
+  "CanvasRenderingContext2D",
+  "DOMMatrix",
+  "ImageData",
+  "TextMetrics",
+  "ImageBitmap",
+  "createImageBitmap",
+] as const;
 
 async function importMockWindow() {
   // @ts-expect-error: Missing files
-  const getCanvasWindow = await import('jest-canvas-mock/lib/window.js').then(res => res.default?.default || res.default || res)
+  const getCanvasWindow = await import("jest-canvas-mock/lib/window.js").then(
+    (res) => res.default?.default || res.default || res
+  );
 
-  const canvasWindow = getCanvasWindow({ document: window.document })
+  const canvasWindow = getCanvasWindow({ document: window.document });
 
   apis.forEach((api) => {
-    global[api] = canvasWindow[api]
-    global.window[api] = canvasWindow[api]
-  })
+    global[api] = canvasWindow[api];
+    global.window[api] = canvasWindow[api];
+  });
 }
 
-importMockWindow()
+importMockWindow();
 
 afterAll(() => {
-  delete global.jest
-  delete global.window.jest
-})
+  delete global.jest;
+  // delete global.window.jest
+});
 
-export {}
+export {};
